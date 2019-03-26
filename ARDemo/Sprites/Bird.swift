3c997198617a1638ee2f4994f3c8adc90fc3ec6a
Abanoub Nabil <abanoubn91@gmail.com>
2019-03-26 17:53:23 +0200

    setup bird object

--- ARDemo/Sprites/Bird.swift
+++ ARDemo/Sprites/Bird.swift
@@ -11,6 +11,32 @@ import GameplayKit
 
 class Bird : SKSpriteNode {
  
+    var mainSprite : SKSpriteNode!
+    
+    func setupBird() {
+        mainSprite = SKSpriteNode(imageNamed: "bird")
+        self.addChild(mainSprite)
+        
+        let textureAtlas = SKTextureAtlas(named: "bird")
+        let frames = ["sprite_0","sprite_1","sprite_2","sprite_3","sprite_4","sprite_5","sprite_6"].map { textureAtlas.textureNamed($0)}
+        let atlasAnimation = SKAction.animate(withNormalTextures: frames, timePerFrame: 1/7, resize: true, restore: false)
+        let animationAction = SKAction.repeatForever(atlasAnimation)
+        mainSprite.run(animationAction)
+        
+        let left = GKRandomSource.sharedRandom().nextBool()
+        
+        if left {
+            mainSprite.xScale = -1
+        }
+        
+        let duration = randomNumber(lowerBound: 15, upperBound: 20)
+        let fade = SKAction.fadeOut(withDuration: TimeInterval(duration))
+        let removeBird = SKAction.run {
+            self.removeFromParent()
+        }
+        let flySequence = SKAction.sequence([fade, removeBird])
+        mainSprite.run(flySequence)
+    }
 }
 
 
